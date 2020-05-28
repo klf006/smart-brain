03059bcd1b581f700090d24c39efc8ef3afcecfe
klf006 <49726847+klf006@users.noreply.github.com>
2020-05-28 17:23:31 -0500

    added particles and styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params = {particlesOptions}
+      />
       <Navigation />
       <Logo />
-      {/*<ImageLinkForm />
-      <FaceRecognition />*/}
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecognition />*/}
        
     </div>
   );
